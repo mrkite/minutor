3d27504eb1454e80e5074298f5deca4c4ec37937
Sean Kasun <skasun@gmail.com>
2013-05-03 13:47:53 -0700

    updated windows installer

--- make_installer.bat
+++ make_installer.bat
@@ -1,3 +1,5 @@
+"c:\Program Files (x86)\Windows Installer XML v3.5\bin\candle.exe" MyInstallDirDlg.wxs
+"c:\Program Files (x86)\Windows Installer XML v3.5\bin\candle.exe" MyWixUI_InstallDir.wxs
 "c:\Program Files (x86)\Windows Installer XML v3.5\bin\candle.exe" minutor.wxs
-"c:\Program Files (x86)\Windows Installer XML v3.5\bin\light.exe" -ext WixUIExtension minutor.wixobj
+"c:\Program Files (x86)\Windows Installer XML v3.5\bin\light.exe" -ext WixUIExtension -o minutor.msi minutor.wixobj MyInstallDirDlg.wixobj MyWixUI_InstallDir.wixobj
 @pause
