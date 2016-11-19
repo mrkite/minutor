68b9ce57bfba1766f6a3a0a7e80649cbdc386463
EtlamGit <etlam@nurfuerspam.de>
2016-11-19 21:41:45 +0100

    migrate Block and Biome to generic color names
    Up to now only the Entities used the flexible CSS standardized color
    format. This commit brings this also to Blocks and Bioms.
    + now using QColor
    + replaced complicated color parsing code with QColor::setNamedColor()
    method
    + simplified calculation of light spectrum
    (Transparent blocks are brighter now. This seems to be a bug in the
    current version, as transparent colors are darkened.)

--- biomeidentifier.cpp
+++ biomeidentifier.cpp
@@ -1,5 +1,7 @@
 /** Copyright (c) 2013, Sean Kasun */
 
+#include <assert.h>
+
 #include "./biomeidentifier.h"
 #include "./json.h"
 
@@ -58,47 +60,31 @@ int BiomeIdentifier::addDefinitions(JSONArray *defs, int pack) {
     else
       biome->name = "Unknown";
 
+    // check for "alpha" (0: transparent / 1: saturated)
+    // probably never used
+    if (b->has("alpha"))
+      biome->alpha = b->at("alpha")->asNumber();
+    else
+      biome->alpha = 1.0;
+
+    // get color definition
+    QColor biomecolor;
     if (b->has("color")) {
-      QString color = b->at("color")->asString();
-      quint32 col = 0;
-      for (int h = 0; h < color.length(); h++) {
-        ushort c = color.at(h).unicode();
-        col <<= 4;
-        if (c >= '0' && c <= '9')
-          col |= c - '0';
-        else if (c >= 'A' && c <= 'F')
-          col |= c - 'A' + 10;
-        else if (c >= 'a' && c <= 'f')
-          col |= c - 'a' + 10;
-      }
-      int rd = col >> 16;
-      int gn = (col >> 8) & 0xff;
-      int bl = col & 0xff;
-
-      if (b->has("alpha"))
-        biome->alpha = b->at("alpha")->asNumber();
-      else
-        biome->alpha = 1.0;
-
-      // pre multiply alphas
-      rd *= biome->alpha;
-      gn *= biome->alpha;
-      bl *= biome->alpha;
-
-      // pre-calculate light spectrum
-      double y = 0.299 * rd + 0.587 * gn + 0.114 * bl;
-      double u = (bl - y) * 0.565;
-      double v = (rd - y) * 0.713;
-      double delta = y / 15;
-      for (int i = 0; i < 16; i++) {
-        y = i * delta;
-        rd = (unsigned int)clamp(y + 1.403 * v, 0, 255);
-        gn = (unsigned int)clamp(y - 0.344 * u - 0.714 * v, 0, 255);
-        bl = (unsigned int)clamp(y + 1.770 * u, 0, 255);
-        biome->colors[i] = (rd << 16) | (gn << 8) | bl;
-      }
+      QString colorname = b->at("color")->asString();
+      biomecolor.setNamedColor(colorname);
+      assert(biomecolor.isValid());
     } else {
-      biome->alpha = 0.0;
+      // use hashed by name instead
+      quint32 hue = qHash(biome->name);
+      biomecolor.setHsv(hue % 360, 255, 255);
+    }
+
+    // pre-calculate light spectrum
+    for (int i = 0; i < 16; i++) {
+      biome->colors[i].setHsv( biomecolor.hue(),
+                               biomecolor.saturation(),
+                               biomecolor.value()*(i/16.0),
+                               255*biome->alpha );
     }
 
     biomes[id].append(biome);
