99fb45b4e9963523aee9cc357ec6d15ebe9fdc7b
EtlamGit <etlam@nurfuerspam.de>
2016-12-26 18:31:46 +0100

    add special color getter methods to BiomeInfo

--- biomeidentifier.cpp
+++ biomeidentifier.cpp
@@ -1,9 +1,16 @@
-/** Copyright (c) 2013, Sean Kasun */
+/*
+Copyright (c) 2013, Sean Kasun
+Copyright (c) 2016, EtlamGit
+*/
 
 #include <assert.h>
 
 #include "./biomeidentifier.h"
 #include "./json.h"
+
+// --------- --------- --------- ---------
+// BiomeInfo
+// --------- --------- --------- ---------
 
 static BiomeInfo unknownBiome;
 
@@ -11,11 +18,108 @@ BiomeInfo::BiomeInfo()
   : name("Unknown Biome")
   , enabled(false)
   , watermodifier(255,255,255)
+  , enabledwatermodifier(false)
   , alpha(1.0)
   , temperature(0.5)
   , humidity(0.5)
 {}
 
+
+// Biome based Grass and Foliage color is based on an interpolation in special
+// textures provided in the Minecraft assets. It is also possible to do the
+// interpolation based only on the color values at the threee corners of that
+// color triangle.
+
+// .\assets\minecraft\textures\colormap\grass.png
+// last checked with Minecraft 1.11.2
+BiomeInfo::T_BiomeCorner BiomeInfo::grassCorners[3] =
+{
+  { 191, 183,  85 },  // lower left  - temperature = 1.0
+  { 128, 180, 151 },  // lower right - temperature = 0.0
+  {  71, 205,  51 }   // upper left  - humidity = 1.0
+};
+
+// .\assets\minecraft\textures\colormap\foliage.png
+// last checked with Minecraft 1.11.2
+BiomeInfo::T_BiomeCorner BiomeInfo::foliageCorners[3] =
+{
+  { 174, 164,  42 },  // lower left  - temperature = 1.0
+  {  96, 161, 123 },  // lower right - temperature = 0.0
+  {  26, 191,   0 }   // upper left  - humidity = 1.0
+};
+
+// will be in STD with C++17
+template <typename T>
+T clamp(const T& n, const T& lower, const T& upper) {
+  return (n<lower)? lower : (n>upper)? upper : n;
+}
+
+/*
+This method is inspired from Mineways, Copyright (c) 2014, Eric Haines
+https://github.com/erich666/Mineways/blob/master/Win/biomes.h
+*/
+// do an interpolation inside the color triangle given by the corners
+// temperature: is the base value for the current Biome
+// humidity:    is the base value for the current Biome
+// elevation:   is number of meters above a height of 64
+QColor BiomeInfo::getBiomeColor( float temperature, float humidity, int elevation, T_BiomeCorner *corners )
+{
+  // check elevation
+  elevation = (elevation < 0) ? 0 : elevation;
+
+  // get local temperature and humidity
+  temperature = clamp( temperature - (float)elevation*0.00166667f, 0.0f, 1.0f );
+  humidity    = clamp( humidity, 0.0f, 1.0f );
+  humidity   *= temperature;  // scale by temperature to stay in triangle
+
+  // lambda values for barycentric coordinates
+  float lambda[3];
+  lambda[0] = temperature - humidity;
+  lambda[1] = 1.0f - temperature;
+  lambda[2] = humidity;
+
+  float red = 0.0f, green = 0.0f, blue = 0.0f;
+  for ( int i = 0; i < 3; i++ )
+  {
+    red   += lambda[i] * corners[i].red;
+    green += lambda[i] * corners[i].green;
+    blue  += lambda[i] * corners[i].blue;
+  }
+
+  int r = (int)clamp( red,   0.0f, 255.0f );
+  int g = (int)clamp( green, 0.0f, 255.0f );
+  int b = (int)clamp( blue,  0.0f, 255.0f );
+
+  return QColor::QColor(r,g,b);
+}
+
+QColor BiomeInfo::getBiomeGrassColor( int elevation )
+{
+  return getBiomeColor( this->temperature, this->humidity, elevation, grassCorners );
+}
+
+QColor BiomeInfo::getBiomeFoliageColor( int elevation )
+{
+  return getBiomeColor( this->temperature, this->humidity, elevation, foliageCorners );
+}
+
+QColor BiomeInfo::getBiomeWaterColor( QColor watercolor )
+{
+  if (this->enabledwatermodifier) {
+    // calculate modified color components
+    int r = (int)( watercolor.red()   * watermodifier.red()   / 255 );
+    int g = (int)( watercolor.green() * watermodifier.green() / 255 );
+    int b = (int)( watercolor.blue()  * watermodifier.blue()  / 255 );
+    // return combined modified color
+    return QColor::QColor(r, g, b );
+  } else
+    return watercolor;
+}
+
+
+// --------- --------- --------- ---------
+// BiomeIdentifier
+// --------- --------- --------- ---------
 
 BiomeIdentifier::BiomeIdentifier() {
   unknownBiome.name = "Unknown";
@@ -86,6 +190,7 @@ int BiomeIdentifier::addDefinitions(JSONArray *defs, int pack) {
     // get watermodifier definition
     if (b->has("watermodifier")) {
       biome->watermodifier.setNamedColor(b->at("watermodifier")->asString());
+      biome->enabledwatermodifier = true;
       assert(biome->watermodifier.isValid());
     }
 
--- biomeidentifier.h
+++ biomeidentifier.h
@@ -8,16 +8,36 @@
 #include <QColor>
 class JSONArray;
 
+
 class BiomeInfo {
+  // public methods
  public:
   BiomeInfo();
+  QColor getBiomeGrassColor  ( int elevation );
+  QColor getBiomeFoliageColor( int elevation );
+  QColor getBiomeWaterColor( QColor watercolor );
+
+  // public members
+ public:
   QString name;
   bool enabled;
   QColor colors[16];
   QColor watermodifier;
+  bool   enabledwatermodifier;
   double alpha;
   double temperature;
   double humidity;
+
+  // private methods and members
+ private:
+  typedef struct T_BiomeCorner {
+    int red;
+    int green;
+    int blue;
+  } T_BiomeCorner;
+  static T_BiomeCorner grassCorners[3];
+  static T_BiomeCorner foliageCorners[3];
+  static QColor getBiomeColor( float temperature, float humidity, int elevation, T_BiomeCorner *corners );
 };
 
 class BiomeIdentifier {
