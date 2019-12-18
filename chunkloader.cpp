e19168ff3748f0acd3c7f5c8859f90db966b9317
EtlamGit <etlam@nurfuerspam.de>
2019-12-18 20:10:21 +0100

    make loadNbt static to reuse it in worldsave

--- chunkloader.cpp
+++ chunkloader.cpp
@@ -15,62 +15,71 @@ ChunkLoader::~ChunkLoader()
 {}
 
 void ChunkLoader::run() {
-  loadNbt();
+  // get existing Chunk entry from Cache
+  QSharedPointer<Chunk> chunk(cache.fetchCached(cx, cz));
+  // load & parse NBT data
+  loadNbt(path, cx, cz, chunk);
   emit loaded(cx, cz);
 }
 
-void ChunkLoader::loadNbt()
+bool ChunkLoader::loadNbt(QString path, int cx, int cz, QSharedPointer<Chunk> chunk)
 {
-  // get coordinates of Region file
+  // check if chunk is a valid storage
+  if (!chunk) {
+    return false;
+  }
+
+  // get coordinates of region file
   int rx = cx >> 5;
   int rz = cz >> 5;
 
-  QFile f(path + "/region/r." + QString::number(rx) + "." +
-          QString::number(rz) + ".mca");
-  if (!f.open(QIODevice::ReadOnly)) {  // no chunks in this region
-    return;
+  QFile f(path + "/region/r." + QString::number(rx) + "." + QString::number(rz) + ".mca");
+  if (!f.open(QIODevice::ReadOnly)) {
+    // no chunks in this region (region file not present at all)
+    return false;
   }
 
   const int headerSize = 4096;
 
   if (f.size() < headerSize) {
-    return; // file header not yet fully written by minecraft
+    // file header not yet fully written by minecraft
+    return false;
   }
 
   // map header into memory
   uchar *header = f.map(0, headerSize);
   int offset = 4 * ((cx & 31) + (cz & 31) * 32);
 
-  int coffset = (header[offset] << 16) | (header[offset + 1] << 8) |
-      header[offset + 2];
+  int coffset = (header[offset] << 16) | (header[offset + 1] << 8) | header[offset + 2];
   int numSectors = header[offset+3];
   f.unmap(header);
 
-  if (coffset == 0) {  // no chunk
+  if (coffset == 0) {
+    // no Chunk information stored in region file
     f.close();
-    return;
+    return false;
   }
 
   const int chunkStart = coffset * 4096;
   const int chunkSize = numSectors * 4096;
 
   if (f.size() < (chunkStart + chunkSize)) {
-    return; // chunk not yet fully written by minecraft
+    // Chunk not yet fully written by Minecraft
+    return false;
   }
 
   uchar *raw = f.map(chunkStart, chunkSize);
   if (raw == NULL) {
     f.close();
-    return;
+    return false;
   }
-  // get existing Chunk entry from Cache
-  QSharedPointer<Chunk> chunk(cache.fetchCached(cx, cz));
   // parse Chunk data
   // Chunk will be flagged "loaded" in a thread save way
-  if (chunk) {
-    NBT nbt(raw);
-    chunk->load(nbt);
-  }
+  NBT nbt(raw);
+  chunk->load(nbt);
   f.unmap(raw);
   f.close();
+
+  // if we reach this point, everything went well
+  return true;
 }
