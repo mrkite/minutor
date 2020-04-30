5b4bfa0ffd7c4f6ec8e3ccdecbeb7c507c0340fa
Nick Korotysh <kolchaprogrammer@list.ru>
2020-04-30 09:56:59 +0300

    add missing 'override' keyword
    fixes a dozens of clang warnings like this:
    ```
    ../minutor/./nbt.h:174:7: warning: 'length' overrides a member function but is not marked 'override' [-Winconsistent-missing-override]
      int length() const;
          ^
    ../minutor/./nbt.h:32:15: note: overridden virtual function is here
      virtual int length() const;
                  ^
    ```
    
    also drop unnecessary 'virtual' keyword
    
    warning was produced because one of methods has 'override' keyword while other are not

--- nbt.h
+++ nbt.h
@@ -58,10 +58,10 @@ class NBT {
 class Tag_Byte : public Tag {
  public:
   explicit Tag_Byte(TagDataStream *s);
-  signed   int toInt() const;
+  signed   int toInt() const override;
   unsigned int toUInt() const;
-  virtual const QString toString() const;
-  virtual const QVariant getData() const;
+  const QString toString() const override;
+  const QVariant getData() const override;
  private:
   quint8 data;
 };
@@ -69,10 +69,10 @@ class Tag_Byte : public Tag {
 class Tag_Short : public Tag {
  public:
   explicit Tag_Short(TagDataStream *s);
-  signed   int toInt() const;
+  signed   int toInt() const override;
   unsigned int toUInt() const;
-  virtual const QString toString() const;
-  virtual const QVariant getData() const;
+  const QString toString() const override;
+  const QVariant getData() const override;
  private:
   qint16 data;
 };
@@ -80,10 +80,10 @@ class Tag_Short : public Tag {
 class Tag_Int : public Tag {
  public:
   explicit Tag_Int(TagDataStream *s);
-  qint32 toInt() const;
-  double toDouble() const;
-  virtual const QString toString() const;
-  virtual const QVariant getData() const;
+  qint32 toInt() const override;
+  double toDouble() const override;
+  const QString toString() const override;
+  const QVariant getData() const override;
  private:
   qint32 data;
 };
@@ -91,10 +91,10 @@ class Tag_Int : public Tag {
 class Tag_Long : public Tag {
  public:
   explicit Tag_Long(TagDataStream *s);
-  qint32 toInt() const;
-  double toDouble() const;
-  virtual const QString toString() const;
-  virtual const QVariant getData() const;
+  qint32 toInt() const override;
+  double toDouble() const override;
+  const QString toString() const override;
+  const QVariant getData() const override;
  private:
   qint64 data;
 };
@@ -103,9 +103,9 @@ class Tag_Float : public Tag {
  public:
   explicit Tag_Float(TagDataStream *s);
 
-  virtual double toDouble() const;
-  virtual const QString toString() const;
-  virtual const QVariant getData() const;
+  double toDouble() const override;
+  const QString toString() const override;
+  const QVariant getData() const override;
 
  private:
   float data;
@@ -114,9 +114,9 @@ class Tag_Float : public Tag {
 class Tag_Double : public Tag {
  public:
   explicit Tag_Double(TagDataStream *s);
-  double toDouble() const;
-  virtual const QString toString() const;
-  virtual const QVariant getData() const;
+  double toDouble() const override;
+  const QString toString() const override;
+  const QVariant getData() const override;
  private:
   double data;
 };
@@ -125,10 +125,10 @@ class Tag_Byte_Array : public Tag {
  public:
   explicit Tag_Byte_Array(TagDataStream *s);
   ~Tag_Byte_Array();
-  int length() const;
+  int length() const override;
   const std::vector<quint8>& toByteArray() const override;
-  virtual const QString toString() const;
-  virtual const QVariant getData() const;
+  const QString toString() const override;
+  const QVariant getData() const override;
  private:
   std::vector<quint8> data;
   int len;
@@ -137,8 +137,8 @@ class Tag_Byte_Array : public Tag {
 class Tag_String : public Tag {
  public:
   explicit Tag_String(TagDataStream *s);
-  const QString toString() const;
-  virtual const QVariant getData() const;
+  const QString toString() const override;
+  const QVariant getData() const override;
  private:
   QString data;
 };
@@ -147,10 +147,10 @@ class Tag_List : public Tag {
  public:
   explicit Tag_List(TagDataStream *s);
   ~Tag_List();
-  const Tag *at(int index) const;
-  int length() const;
-  virtual const QString toString() const;
-  virtual const QVariant getData() const;
+  const Tag *at(int index) const override;
+  int length() const override;
+  const QString toString() const override;
+  const QVariant getData() const override;
  private:
   QList<Tag *> data;
 };
@@ -159,10 +159,10 @@ class Tag_Compound : public Tag {
  public:
   explicit Tag_Compound(TagDataStream *s);
   ~Tag_Compound();
-  bool has(const QString key) const;
-  const Tag *at(const QString key) const;
-  virtual const QString toString() const;
-  virtual const QVariant getData() const;
+  bool has(const QString key) const override;
+  const Tag *at(const QString key) const override;
+  const QString toString() const override;
+  const QVariant getData() const override;
  private:
   QHash<QString, Tag *> children;
 };
@@ -171,10 +171,10 @@ class Tag_Int_Array : public Tag {
  public:
   explicit Tag_Int_Array(TagDataStream *s);
   ~Tag_Int_Array();
-  int length() const;
+  int length() const override;
   const std::vector<qint32>& toIntArray() const override;
-  virtual const QString toString() const;
-  virtual const QVariant getData() const;
+  const QString toString() const override;
+  const QVariant getData() const override;
  private:
   int len;
   std::vector<qint32> data;
@@ -184,10 +184,10 @@ class Tag_Long_Array : public Tag {
  public:
   explicit Tag_Long_Array(TagDataStream *s);
   ~Tag_Long_Array();
-  int length() const;
+  int length() const override;
   const std::vector<qint64>& toLongArray() const override;
-  virtual const QString toString() const;
-  virtual const QVariant getData() const;
+  const QString toString() const override;
+  const QVariant getData() const override;
  private:
   int len;
   std::vector<qint64> data;
