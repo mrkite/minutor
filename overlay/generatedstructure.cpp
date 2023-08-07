1e63b5b08596738993e0e95b839c5668de7f72de
Nick Korotysh <nick.korotysh@gmail.com>
2023-08-08 00:16:17 +0200

    QRegExp -> QRegularExpression in most places
    QRegExp is deprecated since initial Qt 5 release
    
    the only place that still uses QRegExp is search

--- overlay/generatedstructure.cpp
+++ overlay/generatedstructure.cpp
@@ -1,5 +1,6 @@
 /** Copyright 2014 Rian Shelley */
 #include <QPainter>
+#include <QRegularExpression>
 
 #include "overlay/generatedstructure.h"
 #include "nbt/nbt.h"
@@ -100,7 +101,7 @@ GeneratedStructure::tryParseSpawner(const Tag* tagSpawner) {
     mobType = tagSpawner->at("EntityId")->toString();
     // reformat CamelCase text to flattening
     // split at uppercase characters
-    QStringList tokens = mobType.split(QRegExp("(?<=[a-z])(?=[A-Z])"), QString::SkipEmptyParts);
+    QStringList tokens = mobType.split(QRegularExpression("(?<=[a-z])(?=[A-Z])"), QString::SkipEmptyParts);
     mobType = tokens.join("_").toLower();
   }
   // after "The Flattening"
